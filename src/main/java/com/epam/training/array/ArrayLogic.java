1c09aeb88c59ebf6d825143258f6288808a5fbfc
AIniuseva <anz.iniu@gmail.com>
2021-04-04 16:38:24 +0300

    Added classes realization: ArrayLogic, ArraySorter

--- src/main/java/com/epam/training/array/ArrayLogic.java
+++ src/main/java/com/epam/training/array/ArrayLogic.java
@@ -3,18 +3,76 @@ package com.epam.training.array;
 public class ArrayLogic {
 
     public boolean exists(Array array, int element) {
-        throw new UnsupportedOperationException();
+        int startIndex = 0;
+        int endIndex = array.getItems().length - 1;
+        while (startIndex <= endIndex) {
+            int middle = startIndex + (endIndex - startIndex) / 2;
+
+            if (array.getItems()[middle] == element) {
+                return true;
+            } else if (array.getItems()[middle] < element) {
+                startIndex = middle + 1;
+            } else {
+                endIndex = middle - 1;
+            }
+        }
+        return false;
     }
 
     public Array findPrimeNumbers(Array array) {
-        throw new UnsupportedOperationException();
+        int newArraySize = 0;
+
+        for (int element : array.getItems()) {
+            if (isPrime(element)) {
+                newArraySize++;
+            }
+        }
+
+        int[] primeNumbersArray = new int[newArraySize];
+
+        int j = 0;
+        for (int element : array.getItems()) {
+            if (isPrime(element)) {
+                primeNumbersArray[j] = element;
+                j++;
+            }
+        }
+
+        return new Array(primeNumbersArray);
+    }
+
+    private boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        } else {
+            for (int i = 2; i < number; i++) {
+                if (number % i == 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     public int findMinValue(Array array) {
-        throw new UnsupportedOperationException();
+        int minValue = array.getItems()[0];
+
+        for (int i = 1; i < array.getItems().length; i++) {
+            if (array.getItems()[i] < minValue) {
+                minValue = array.getItems()[i];
+            }
+        }
+        return minValue;
     }
 
     public int findMaxValue(Array array) {
-        throw new UnsupportedOperationException();
+        int maxValue = array.getItems()[0];
+
+        for (int i = 1; i < array.getItems().length; i++) {
+            if (array.getItems()[i] > maxValue) {
+                maxValue = array.getItems()[i];
+            }
+        }
+        return maxValue;
     }
 }
--- src/main/java/com/epam/training/array/ArraySorter.java
+++ src/main/java/com/epam/training/array/ArraySorter.java
@@ -1,15 +1,63 @@
 package com.epam.training.array;
 
 public class ArraySorter {
+
+    private static final ArraySorter arraySorter = new ArraySorter();
+
     public Array sort(Array array) {
-        throw new UnsupportedOperationException();
+        int newArraySize = 0;
+        for (int value : array.getItems()) {
+            if (value > 0) {
+                newArraySize++;
+            }
+        }
+
+        int[] arrayWithoutNegativeNumbers = new int[newArraySize];
+        int j = 0;
+
+        for (int k : array.getItems()) {
+            if (k > 0) {
+                arrayWithoutNegativeNumbers[j] = k;
+                j++;
+            }
+        }
+        return arraySorter.bubbleSort(new Array(arrayWithoutNegativeNumbers));
     }
 
     public Array sortNegative(Array array) {
-        throw new UnsupportedOperationException();
+        int newArraySize = 0;
+        for (int element : array.getItems()) {
+            if (element < 0) {
+                newArraySize++;
+            }
+        }
+
+        int[] sortedNegativeArray = new int[newArraySize];
+        int j = 0;
+
+        for (int k : array.getItems()) {
+            if (k < 0) {
+                sortedNegativeArray[j] = k;
+                j++;
+            }
+        }
+        return arraySorter.bubbleSort(new Array(sortedNegativeArray));
     }
 
     public Array bubbleSort(Array array) {
-        throw new UnsupportedOperationException();
+        int[] sortedArray = array.getItems();
+        boolean isSorted = false;
+        while (!isSorted) {
+            isSorted = true;
+            for (int i = 1; i < sortedArray.length; i++) {
+                if (sortedArray[i] < sortedArray[i - 1]) {
+                    int temp = sortedArray[i];
+                    sortedArray[i] = sortedArray[i - 1];
+                    sortedArray[i - 1] = temp;
+                    isSorted = false;
+                }
+            }
+        }
+        return new Array(sortedArray);
     }
 }
